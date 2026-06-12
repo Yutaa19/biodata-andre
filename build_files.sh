e9bf9f7d0f3cc8a9b0d87b6368c908c6d7e65e56
Yutaa19 <yutaa1986@gmail.com>
2026-06-12 08:08:57 +0700

    code production

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
